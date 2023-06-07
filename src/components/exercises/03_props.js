70e263f01231d6b6fc45a1dab7051932481e8c69
Isadora Fitzgerald <fitzdora@gmail.com>
2023-06-07 19:04:00 +0100

    Excercise 3 completed

--- src/components/exercises/03_props.js
+++ src/components/exercises/03_props.js
@@ -2,10 +2,45 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
-  return (
-      <h1>TODO</h1>
-  );
+const Demo = (props) => {
+  const course = "M.SC. Enterprise Software Systems";
+  const modules = [
+    {
+    name: "Cloud Architecture",
+    noLectures: 2,
+    noPracticals: 3
+    }, 
+    {
+    name: "Enterprise Web Dev",
+    noLecture: 2,
+    noPracticals: 3
+    }
+  ];
+  return (<div>
+    <h2>{`${props.course} - Modules table`}</h2>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No. Lectures</th>
+          <th>No. Practicals</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{props.modules[0].name}</td>
+          <td>{props.modules[0].noLecture}</td>
+          <td>{props.modules[0].noPracticals}</td>
+        </tr>
+        <tr>
+          <td>{props.modules[1].name}</td>
+          <td>{props.modules[1].noLecture}</td>
+          <td>{props.modules[1].noPracticals}</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+);
 };
 
 export default Demo;
